f1cb5d8645395d09d35d4dc76038368a1456b881
Nathan Perkins <nperkins487@gmail.com>
2019-03-15 10:31:53 -0700

    Modify for junit 4

--- FinalProject/URLValidatorCorrect/test/UrlValidatorTestUnit.java
+++ FinalProject/URLValidatorCorrect/test/UrlValidatorTestUnit.java
@@ -2,34 +2,20 @@
 
 import junit.framework.TestCase;
 
-import java.util.Random;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
 
 
-public class UrlValidatorTestProject extends TestCase {
+public class UrlValidatorTestUnit extends TestCase {
     static final String LOWER_CASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
     static final String UPPER_CASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static final String NUMERIC_CHARS = "0123456789";
 
-    public UrlValidatorTestProject(String testName) {
+    public UrlValidatorTestUnit(String testName) {
         super(testName);
     }
 
-
-
-    public void testManualTest()
-    {
-//You can use this function to implement your manual testing	   
-
-    }
-    
 	boolean softAssertEquals(String message, boolean expected, boolean actual) {
 		if(expected == actual) {
 			//System.out.println("PASS: " + message);
@@ -39,8 +25,7 @@ public class UrlValidatorTestProject extends TestCase {
 		return false;
 	}
 	
-    @Test
-    public void unitTestOne() {
+    public void testUnitOne() {
 		ResultPair[] testUrls = {
 				new ResultPair("http://www.yahoo.com", true),
 				new ResultPair("https://www.teamliquid.net/forum/sc2-tournaments/", true),
@@ -81,8 +66,7 @@ public class UrlValidatorTestProject extends TestCase {
     	assertTrue(result);
     }
     
-    @Test
-    public void unitTestTwo() {
+    public void testUnitTwo() {
     	ResultPair[] schemes = {
     			new ResultPair("http://", true),
     			new ResultPair("https://", true),
@@ -168,98 +152,5 @@ public class UrlValidatorTestProject extends TestCase {
     	
     	System.out.println("Unit tests two passed: " + finalResult);
     	assertTrue(finalResult);
-    }
-    
-    public void testYourFirstPartition()
-    {
-        //You can use this function to implement your First Partition testing
-
-    }
-
-    public void testYourSecondPartition(){
-        //You can use this function to implement your Second Partition testing
-
-    }
-    //You need to create more test cases for your Partitions if you need to
-
-    public void testIsValid()
-    {
-        //You can use this function for programming based testing
-
-    }
-
-    private static String generateString(int length, String validChars, String invalidChars)
-    {
-        StringBuilder s = new StringBuilder();
-        double validRatio     = Math.pow(0.9, 1.0/length);
-
-        for (int i = 0; i < length; i++)
-        {
-            float validOrNot = new Random().nextFloat();
-            int choice;
-
-            if (validOrNot > validRatio) {
-                choice = new Random().nextInt(invalidChars.length());
-                s.append(invalidChars.charAt(choice));
-            } else {
-                choice = new Random().nextInt(validChars.length());
-                s.append(validChars.charAt(choice));
-            }
-        }
-
-        return s.toString();
-    }
-    
-    private static String randomString (String characterPool) {
-    	StringBuilder result = new StringBuilder();
-    	int length = 
-    	for(int i=0; i<characterPool.length(); i++) {
-    		
-    	}
-    }
-
-
-    private static ResultPair generateScheme(int length)
-    /*
-    https://tools.ietf.org/html/rfc3986#section-3.1
-    scheme = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." )
-    */
-
-    {
-        String validChars     = LOWER_CASE_CHARS + UPPER_CASE_CHARS + NUMERIC_CHARS + "+-.";
-        String invalidChars   = "~!@#$%^&*()_";
-
-        String item = generateString(length, validChars, invalidChars);
-        boolean valid = !item.equals("") || item.matches("^[A-z]+[A-z0-9+\\-.]*$");
-
-        return new ResultPair(item, valid);
-    }
-    
-    // Valid format ?foo=bar&second=was+it+clear+%28already%29%3F
-    private static ResultPair generateQuery(int length) {
-    	String item;
-    	boolean valid;
-    	return new ResultPair(item, valid);
-    }
-    
-    private static ResultPair generatePath(int length) {
-    	
-    	String validChars = LOWER_CASE_CHARS + UPPER_CASE_CHARS + NUMERIC_CHARS;
-    	String invalidChars = ;
-    	String item = generateString(length, validChars, invalidChars);
-    	boolean valid;
-    	
-    	return new ResultPair(item, valid);
-    }
-
-    public void testRandom()
-    {
-        ArrayList<ResultPair> schemes = new ArrayList<ResultPair>();
-        for (int i = 0; i < 20; i++)
-        {
-            int length = new Random().nextInt(10);
-            schemes.add(generateScheme(length));
-        }
-        //System.out.println(schemes.size());
     }
 }
