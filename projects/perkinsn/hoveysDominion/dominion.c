614527b5ac01203aab7f164f2d3c8b1af5e6c0f0
Nathan Perkins <nperkins487@gmail.com>
2019-03-16 16:55:38 -0700

    Refactor village to its own function

--- projects/perkinsn/hoveysDominion/dominion.c
+++ projects/perkinsn/hoveysDominion/dominion.c
@@ -596,6 +596,20 @@ int smithyEffect(int currentPlayer, struct gameState *state, int handPos){
   return 0;
 }
 
+int villageEffect(int currentPlayer, struct gameState *state, int handPos)
+{
+  //+1 Card
+  drawCard(currentPlayer, state);
+  
+  //+2 Actions
+  state->numActions = state->numActions + 2;
+  
+  //discard played card from hand
+  discardCard(handPos, currentPlayer, state, 0);
+
+  return 0;
+}
+
 //Adventurer
 int adventurerEffect(struct gameState *state, int currentPlayer, int drawntreasure, int cardDrawn, int z, int temphand[MAX_HAND]){
     //int temphand[MAX_HAND];// moved above the if statement
@@ -896,15 +910,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+      return villageEffect(currentPlayer, state, handPos);
 		
     case baron:
       state->numBuys++;//Increase buys by 1!
