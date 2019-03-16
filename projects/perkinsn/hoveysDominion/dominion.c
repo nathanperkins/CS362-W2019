255825bd599d0e998292edab6b0719f6b3e5df84
Nathan Perkins <nperkins487@gmail.com>
2019-03-16 16:44:50 -0700

    cardDrawn was not looking at the top card

--- projects/perkinsn/hoveysDominion/dominion.c
+++ projects/perkinsn/hoveysDominion/dominion.c
@@ -605,7 +605,7 @@ int adventurerEffect(struct gameState *state, int currentPlayer, int drawntreasu
     	}
     	drawCard(currentPlayer, state);
     	//bug introduced: removed the '-1', this makes it such that the card on the top card in the hand is not the most recently drawn card 
-    	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]];//top card of hand is most recently drawn card.
+    	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] - 1];//top card of hand is most recently drawn card.
     	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
     	  drawntreasure++;
     	else{
